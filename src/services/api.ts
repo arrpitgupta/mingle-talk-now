b794e399e36e4391cb5ffcdcf95c2f305bf03427
arrpitgupta <arpit17200@gmail.com>
2025-07-28 12:30:35 +0530

    some changes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL:'https://mingle-talk-back.vercel.app/api',
+  // baseURL: 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
